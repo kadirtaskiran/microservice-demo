ac59951a4541fbb29c47c0b8479a0effedde6988
kadirtaskiran <iamkadirtaskiran@gmail.com>
2025-03-15 23:51:11 +0300

    Add product service

---
+++ product-service/src/test/java/com/kmt/microservices/product/ProductServiceApplicationTests.java
@@ -0,0 +1,55 @@
+package com.kmt.microservices.product;
+
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ProductServiceApplicationTests {
+
+	@ServiceConnection
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer();
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setUp() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mongoDBContainer.start();
+	}
+
+	@Test
+	void shouldCreateProduct() {
+		String requestBody = """
+				{
+				    "name": "Product2",
+				    "description": "Product2 desc",
+				    "price": 1000
+				}
+				""";
+
+		RestAssured.given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/api/product")
+				.then()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("name", Matchers.equalTo("Product2"))
+				.body("description", Matchers.equalTo("Product2 desc"))
+				.body("price", Matchers.equalTo(1000));
+	}
+
+}
